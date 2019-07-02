e7fb828cc06dd961fabcd62a92d73cce4e8e85b6
Carlos <carlos.zepeda1392@gmail.com>
2019-07-01 23:05:12 -0600

    Se crea la conexino a base de datos
    El nombre de la conesxino es ConexionDB
    
    Para realizar la conexion a la base de datos es con; conectar()
    
    para realizar la desconexion a la base de datos es con; desconectar()

---
+++ DesdApp_Root/src/dao/ConexionDB.java
@@ -0,0 +1,47 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+
+    //atributos
+    private Connection conexionDB;
+    private static final String URL = "jdbc:mysql://127.0.0.1/_poner_el_enlace_a_la_base_de_datos";//poner el enlace a la base de datos con respecto a su configuaracion
+    private static final String USER = "root";//esto de igual manera
+    private static final String PASS = "";//y esto, si no lo han modificado dejen user y pass vacions
+
+    //Metodos
+    public void conectar() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexionDB = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Conexion Establecida");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("NO se establecion la conexion, error: " + e);
+        }
+    }
+
+    public void desconectar() {
+        if (conexionDB != null) {
+            try {
+                if (conexionDB.isClosed() == false) {
+                    conexionDB.close();
+                    System.out.println("Desconecion se realizo con exito");
+                }
+            } catch (SQLException e) {
+                System.out.println("NO de desconectar de la base de datos, error: " + e);
+            }
+        }
+    }
+
+    //Setter and Getter
+    public Connection getconexionDB() {
+        return conexionDB;
+    }
+
+    public void setconexionDB(Connection conexionDB) {
+        this.conexionDB = conexionDB;
+    }
+}
