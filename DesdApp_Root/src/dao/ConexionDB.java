01e62bf9b91feaae20d1c16faa0272b104543835
EstebanRK <+EstebanRK@users.noreply.github.com>
2019-07-18 11:08:56 -0600

    ConexionBD
    Revision de gramatica

--- DesdApp_Root/src/dao/ConexionDB.java
+++ DesdApp_Root/src/dao/ConexionDB.java
@@ -10,7 +10,7 @@ public class ConexionDB {
     private Connection conexionDB;
     private static final String URL = "jdbc:mysql://127.0.0.1/_poner_el_enlace_a_la_base_de_datos";//poner el enlace a la base de datos con respecto a su configuaracion
     private static final String USER = "root";//esto de igual manera
-    private static final String PASS = "";//y esto, si no lo han modificado dejen user y pass vacions
+    private static final String PASS = "";//y esto, si no lo han modificado dejen user y pass vacios
 
     //Metodos
 
@@ -21,19 +21,19 @@ public class ConexionDB {
             conexionDB = DriverManager.getConnection(URL, USER, PASS);//se envia la solicitud a la base de datos mediante los parametros previamente definidos
             System.out.println("Conexion Establecida");//este sout no es necesario ya que solo muestra cuando la conexion es exitos
         } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("NO se establecion la conexion, error: " + e);//Muestra el mensaje de error en la consolo se puede modificar para que se vea en otro lado por ejemplo un log.
+            System.out.println("NO se establecio la conexion, error: " + e);//Muestra el mensaje de error en la consolo se puede modificar para que se vea en otro lado por ejemplo un log.
         }
     }
     //Desconectar de la base de datos
     public void desconectar() {
-        if (conexionDB != null) {//Indicamos que si existe una conexion la cierre de lo contario no hara nada
+        if (conexionDB != null) {//Indicamos que si existe una conexion la cierre, de lo contario no hara nada
             try {
-                if (conexionDB.isClosed() == false) {//conexion.isClosed() devuelve una valor verdadero, con el if estamos verificano que enrealidad exita una conexion.
+                if (conexionDB.isClosed() == false) {//conexion.isClosed() devuelve una valor verdadero, con el if estamos verificano que en realidad exita una conexion.
                     conexionDB.close();
-                    System.out.println("Desconecion se realizo con exito");//Esto de igual manera no es necesario
+                    System.out.println("Desconeccion se realizo con exito");//Esto de igual manera no es necesario
                 }
             } catch (SQLException e) {
-                System.out.println("NO de desconectar de la base de datos, error: " + e); //Muestra el mensaje de error en consola               
+                System.out.println("NO se desconecto de la base de datos, error: " + e); //Muestra el mensaje de error en consola               
             }
         }
     }
