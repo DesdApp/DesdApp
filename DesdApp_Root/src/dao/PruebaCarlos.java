b561ee834c6aa49d3d6d2d4d99e97a10e82d735a
Carolinavelasquez <ekarol1994@gmail.com>
2019-07-26 12:20:23 -0600

    Update PruebaCarlos.java

--- DesdApp_Root/src/dao/PruebaCarlos.java
+++ DesdApp_Root/src/dao/PruebaCarlos.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import modelo.Empleado;
+import modelo.EstadosPropiedades;
 import modelo.TipoUsuario;
 
 /**
@@ -19,12 +20,198 @@ import modelo.TipoUsuario;
 public class PruebaCarlos {
 
     public static void main(String[] args) {
-        ConexionDB cn = new ConexionDB();
 
+        // Instanciamos las clases
+//        ArrayList list;
+        Date date = new Date(12202272000L * 1000);
         Scanner sc = new Scanner(System.in);
-        int opcion = sc.nextInt();
-                
-        switch (opcion) {
+
+        // Instancias de los modelos
+        Empleado emp = new Empleado(7, 6, "Prueba", date, date, (double) 1000, "UserPrueba", "Pss Prueba", 1, 1);
+        EstadosPropiedades estadoProp= new EstadosPropiedades((byte)5,"Prueba");
+        
+        
+
+        Empleado datosEmp=new Empleado();
+        EstadosPropiedades datoEstados=new EstadosPropiedades();
+        
+
+        // Instancias de los DAO
+        DAOEmpleado daoEmp = new DAOEmpleado();
+        DAOEstPropiedades daoEst=new DAOEstPropiedades();
+        DAODetVentas daoDetalle = new DAODetVentas();
+        DAOVentas daoVenta = new DAOVentas();
+        DAOPagosPropiedades daoPago = new DAOPagosPropiedades();
+        DAOTpPagos daoTipo = new DAOTpPagos();
+
+        // Realizamos la conexion
+        ConexionDB cx = new ConexionDB();
+
+        // Atributos
+        int id;
+
+        System.out.println("1) Realizar pruebas en Empleados");
+        System.out.println("2) Realizar pruebas en Propietarios");
+        System.out.println("3) Realizar pruebas en Estado de Propiedades");
+        System.out.println("4) Realizar pruebas en TiposPagos");
+        System.out.println("5) Salir");
+        id = sc.nextInt();
+        switch (id) {
+            //<editor-fold defaultstate="collapsed" desc="Pruebas DetallesVentas">
+            case 1:
+                System.out.println("1) INSERT");
+                System.out.println("2) DELETE");
+                System.out.println("3) UPDATE");
+                System.out.println("4) SELECT");
+                System.out.println("5) LIST");
+                System.out.println("6) Regresar");
+                id = sc.nextInt();
+                switch (id) {
+                    case 1:
+                        daoEmp.insertEmpleado(emp);
+
+                        break;
+
+                    case 2:
+                        daoEmp.deleteEmpleado(id);
+                        break;
+
+                    case 3:
+                        daoEmp.updateEmpleado(emp);
+                        break;
+
+                    case 4:
+                        daoEmp.selectEmpleado(id);
+                        break;
+
+                    case 5:
+                        ArrayList<Empleado> lista = new ArrayList<>();
+                        lista = daoEmp.ListEmpleado();
+                        for (Empleado empleado : lista) {
+                            System.out.println(empleado.toString());
+                        }
+                        break;
+
+                    case 6:
+                        break;
+
+                    default:
+                        throw new AssertionError();
+                }
+                break;
+            //</editor-fold>
+
+            case 2:
+                System.out.println("1) INSERT");
+                System.out.println("2) DELETE");
+                System.out.println("3) UPDATE");
+                System.out.println("4) SELECT");
+                System.out.println("5) LIST");
+                System.out.println("6) Regresar");
+                id = sc.nextInt();
+                switch (id) {
+                    case 1:
+                        System.out.println("Aun no esta el modelo la interfaz y el dao");
+                        break;
+                    case 2:
+                        System.out.println("Aun no esta el modelo la interfaz y el dao");
+                        break;
+                    case 3:
+                        System.out.println("Aun no esta el modelo la interfaz y el dao");
+                        break;
+                    case 4:
+                        System.out.println("Aun no esta el modelo la interfaz y el dao");
+                            break;
+                    case 5:
+                        System.out.println("Aun no esta el modelo la interfaz y el dao");
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        throw new AssertionError();
+                }
+                //<editor-fold defaultstate="collapsed" desc="Pruebas Ventas">
+
+                //</editor-fold>
+                break;
+
+            case 3:
+                System.out.println("1) INSERT");
+                System.out.println("2) DELETE");
+                System.out.println("3) UPDATE");
+                System.out.println("4) SELECT");
+                System.out.println("5) LIST");
+                System.out.println("6) Regresar");
+                id = sc.nextInt();
+                switch (id) {
+                    case 1:
+
+                        break;
+                        case 2:
+
+                        break;
+                        case 3:
+
+                        break;
+                        case 4:
+
+                        break;
+                        case 5:
+
+                        break;
+                        case 6:
+
+                        break;
+                    default:
+                        throw new AssertionError();
+                }
+                //<editor-fold defaultstate="collapsed" desc="Pruebas PagosPropiedades">
+
+                //</editor-fold>
+                break;
+
+            case 4:
+                System.out.println("1) INSERT");
+                System.out.println("2) DELETE");
+                System.out.println("3) UPDATE");
+                System.out.println("4) SELECT");
+                System.out.println("5) LIST");
+                System.out.println("6) Regresar");
+                id = sc.nextInt();
+                switch (id) {
+                    case 1:
+
+                        break;
+                    default:
+                        throw new AssertionError();
+                }
+                //<editor-fold defaultstate="collapsed" desc="Pruebas TiposPagos">
+
+                //</editor-fold>
+                break;
+
+            case 5:
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+    
+    
+    
+    /*
+
+    ConexionDB cn = new ConexionDB();
+
+    Scanner sc = new Scanner(System.in);
+    int opcion = sc.nextInt();
+
+    switch (opcion
+
+    
+    
+
+) {
 
             case 1:
                 
@@ -134,4 +321,4 @@ public class PruebaCarlos {
         }*/
     }
 
-}
+
