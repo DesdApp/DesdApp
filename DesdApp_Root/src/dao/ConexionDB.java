ea9b4891c7ebf53fd88a11f861ac5a8acd528a56
Carlos <carlos.zepeda1392@gmail.com>
2019-07-30 09:32:17 -0600

    Se elimina por no ser necesario
    Solo fue una prueba

--- DesdApp_Root/src/dao/ConexionDB.java
+++ DesdApp_Root/src/dao/ConexionDB.java
@@ -52,6 +52,3 @@ public class ConexionDB {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
-
-
-//esto no tiene relevancia
