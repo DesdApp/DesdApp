29ebe89fc981f9ddf59611cb92e22a0d04d781af
DesdApp <desdapp2019developers@gmail.com>
2019-08-27 01:40:38 -0600

    Update DAOCorredores.java
    doc. Corredores javadoc

--- DesdApp_Web/src/java/dao/DAOCorredores.java
+++ DesdApp_Web/src/java/dao/DAOCorredores.java
@@ -1,16 +1,30 @@
 package dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import modelo.Corredores;
 
+/**
+ * Esta clase es el objeto de acceso a datos para la clase Corredores.
+ *
+ * @author carolina
+ */
 public class DAOCorredores implements interfaces.corredoresInterface{
+    //Instanciamos las clases
     ConexionDB conex = new ConexionDB();
     PreparedStatement execute;
+    //Atributos
     ResultSet rs;
     String sql;
     
-    
+    /**
+     * Este metodo es para obtener todos los registros de la base de datos
+     * correspondientes a la clase Corredores.
+     *@param cod_emp este parametro 
+     * @return retornará una lista de los registros de la tabla que se pide en
+     * la consulta
+     */
     @Override
     public ArrayList<Corredores> lista(int cod_emp) {
         ArrayList<Corredores> lista = new ArrayList<>();
@@ -37,7 +51,7 @@ public class DAOCorredores implements interfaces.corredoresInterface{
             lista.add(cor);
             }
             rs.close();
-        } catch (Exception e) {
+        } catch (SQLException e) {
             System.out.println("Error en DAOCorredores: " + e.getMessage());
         }finally{
             conex.desconectar();
